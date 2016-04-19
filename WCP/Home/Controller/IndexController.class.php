33083c975de6260b42b49db659d8048f898c61c7
helen <731873664@qq.com>
2016-04-19 16:12:42 +0800

    项目入口控制器

--- WCP/Home/Controller/IndexController.class.php
+++ WCP/Home/Controller/IndexController.class.php
@@ -11,10 +11,30 @@ class IndexController extends Controller
     //项目入口跳转行为(访问人员记录)
     public function index()
     {
-        dump('hello,CMS');
-        //访问人员记录、时间、IP等
 
-        //页面重定向
-        redirect(U('Home/Index/Display/main'), 0, '信息');
+        //sleep for 5 seconds
+        //sleep(5);
+
+        //存储访问用户记录（访问人员记录、时间、IP等）
+        $res = save_browse_user_records();
+        if ($res) {
+            //页面重定向
+            redirect(U('Home/Index/Display/main'), 0);
+        } else {
+            $this->save_errmsg_records();
+            //页面重定向
+            redirect(U('Home/Index/Display/main'), 0);
+        }
+
     }
+
+    /*
+     * 存储错误记录函数
+     *
+     * */
+    function save_errmsg_records()
+    {
+
+    }
+
 }
