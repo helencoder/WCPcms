a51fc3a66a2ac1741ef6f94e1015ade6709db4b2
helen <731873664@qq.com>
2016-04-19 16:25:45 +0800

    用户管理类

---
+++ WCP/Home/Controller/Admin/UserController.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Author: helen
+ * CreateTime: 2016/4/19 16:24
+ * description: 用户管理类
+ */
+namespace Home\Controller\Admin;
+
+use Home\Controller\CommonController;
+
+class UserController extends CommonController
+{
+
+}
