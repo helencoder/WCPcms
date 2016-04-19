306bb229131c9eda5defbb34793bcfcff452054c
helen <731873664@qq.com>
2016-04-19 17:40:59 +0800

    自定义错误处理函数

--- WCP/Home/Common/function.php
+++ WCP/Home/Common/function.php
@@ -43,7 +43,6 @@ function save_browse_user_records()
     }
     return $res;
 }
-
 /*
  * 获取用户ip，避免使用代理情况，获取真实IP
  * */
@@ -70,4 +69,50 @@ function getip()
     if (false !== strpos($ip, ','))
         $ip = reset(explode(',', $ip));
     return $ip;
+}
+
+/*
+ * 自定义错误处理函数
+ * 功能：记录错误信息表，返回相关信息
+ * 参数	描述
+    error_level	    必需。为用户定义的错误规定错误报告级别。必须是一个值数。
+    error_message	必需。为用户定义的错误规定错误消息。
+    error_file	    可选。规定错误在其中发生的文件名。
+    error_line	    可选。规定错误发生的行号。
+    error_context	可选。规定一个数组，包含了当错误发生时在用的每个变量以及它们的值。
+ *
+ * */
+/*
+ * 文件打开模式(fopen、fclose)
+ * 模式	描述
+    r	打开文件为只读。文件指针在文件的开头开始。
+    w	打开文件为只写。删除文件的内容或创建一个新的文件，如果它不存在。文件指针在文件的开头开始。
+    a	打开文件为只写。文件中的现有数据会被保留。文件指针在文件结尾开始。创建新的文件，如果文件不存在。
+    x	创建新文件为只写。返回 FALSE 和错误，如果文件已存在。
+    r+	打开文件为读/写、文件指针在文件开头开始。
+    w+	打开文件为读/写。删除文件内容或创建新文件，如果它不存在。文件指针在文件开头开始。
+    a+	打开文件为读/写。文件中已有的数据会被保留。文件指针在文件结尾开始。创建新文件，如果它不存在。
+    x+	创建新文件为读/写。返回 FALSE 和错误，如果文件已存在。
+ * */
+
+function customError($error_level, $error_message, $error_file, $error_line, $error_context)
+{
+    $time = date('Y-m-d H:i:s', time());
+    $errmsg = $time . " <b>Error:</b> [$error_level] $error_message<br />" . "error_file:$error_file" . "($error_line)。\r\n\r\n";
+    $date = date('Y-m-d', time());
+    $root = $_SERVER['DOCUMENT_ROOT'];
+    $filename = $root . '/WCPcms/Log/' . "$date" . '.log';
+    $dir = $root . '/WCPcms/Log';
+    if (!file_exists($dir)) {
+        @mkdir($dir, 0777);
+    }
+    //错误日志记录,文件不存在，则直接创建后添加信息；否则直接追加
+    if (!file_exists($filename)) {
+        touch($filename);
+        @$fp = fopen($filename, "a");
+        fwrite($fp, $errmsg);
+        fclose($fp);
+    } else {
+        error_log($errmsg, 3, $filename);
+    }
 }
