718e7b0b507ba54ea241945d8d7ae1038994252c
helencoder <731873664@qq.com>
2016-06-01 22:28:13 +0800

    数据库配置文件

--- WCP/Common/Conf/db.php
+++ WCP/Common/Conf/db.php
@@ -6,7 +6,7 @@ return array(
     'DB_HOST' => '127.0.0.1', // 服务器地址
     'DB_NAME' => 'wcp',          // 数据库名
     'DB_USER' => 'root',      // 用户名
-    'DB_PWD' => 'root',          // 密码
+    'DB_PWD' => '',          // 密码
     'DB_PORT' => '3306',        // 端口
     'DB_PREFIX' => 'wcp_',    // 数据库表前缀
     /*'DB_PARAMS' => array(), // 数据库连接参数
