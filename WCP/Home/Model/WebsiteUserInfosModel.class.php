0b293c2861a65e020d994a164db148a875f09113
helencoder <731873664@qq.com>
2016-06-07 16:46:18 +0800

    网站用户信息表

---
+++ WCP/Home/Model/WebsiteUserInfosModel.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Author: helen
+ * CreateTime: 2016/06/07 16:40
+ * Description: 网站用户信息表
+ */
+namespace WCP\Model;
+
+use Think\Model;
+
+class WebsiteUserInfosModel extends Model
+{
+    protected $tablePrefix = 'wcp_';
+
+}
