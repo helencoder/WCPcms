007c1d76bfe9bc04dcc17fa85e61594a099f27ff
helencoder <731873664@qq.com>
2016-06-07 17:02:33 +0800

    错误记录表

---
+++ WCP/Home/Model/ErrorRecordsModel.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Author: helen
+ * CreateTime: 2016/06/07 17:01
+ * Description: 错误记录表
+ */
+namespace WCP\Model;
+
+use Think\Model;
+
+class ErrorRecordsModel extends Model
+{
+    protected $tablePrefix = 'wcp_';
+
+}
