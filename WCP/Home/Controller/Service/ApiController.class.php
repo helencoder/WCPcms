64b5c34c85a3ce09c4d5be7228e987a0fcfcceaf
helencoder <731873664@qq.com>
2016-06-07 16:34:09 +0800

    API控制器

---
+++ WCP/Home/Controller/Service/ApiController.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Author: helen
+ * CreateTime: 2016/06/07 16:33
+ * Description: API控制器
+ */
+namespace Home\Controller\Service;
+
+use Home\Controller\CommonController;
+
+class ApiController extends CommonController
+{
+
+}
