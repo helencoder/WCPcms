399b22504d7f2fba1b3c21e2388841233ea181d7
helencoder <731873664@qq.com>
2016-06-07 16:24:31 +0800

    自定义菜单控制器

--- WCP/Home/Controller/Weixin/MenuController.class.php
+++ WCP/Home/Controller/Weixin/MenuController.class.php
@@ -2,5 +2,16 @@
 /**
  * Author: helen
  * CreateTime: 2016/06/07 16:22
- * Description:
+ * Description: 自定义菜单控制器
  */
+namespace Home\Controller\Weixin;
+
+use Home\Controller\CommonController;
+
+class MenuController extends CommonController
+{
+    public function index()
+    {
+
+    }
+}
