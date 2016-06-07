c270b341f6bf1c5f8f44dd1dfb0af72b020aa1c3
helencoder <731873664@qq.com>
2016-06-07 16:26:42 +0800

    微信素材控制器

---
+++ WCP/Home/Controller/Weixin/MaterialController.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Author: helen
+ * CreateTime: 2016/06/07 16:25
+ * Description: 微信素材控制器
+ */
+namespace Home\Controller\Weixin;
+
+use Home\Controller\CommonController;
+
+class MaterialController extends CommonController
+{
+    public function index()
+    {
+
+    }
+}
