0d2d3874c063451367723bab65efac27628de97c
helencoder <731873664@qq.com>
2016-06-07 17:00:20 +0800

    访问用户记录表

---
+++ WCP/Home/Model/BrowseUserRecordsModel.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Author: helen
+ * CreateTime: 2016/06/07 16:59
+ * Description: 访问用户记录表
+ */
+namespace WCP\Model;
+
+use Think\Model;
+
+class BrowseUserRecordsModel extends Model
+{
+    protected $tablePrefix = 'wcp_';
+
+}
