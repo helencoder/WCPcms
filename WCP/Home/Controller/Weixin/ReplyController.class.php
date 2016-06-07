b6c4d7f8ab75f0e0dfb4b544be90924321500377
helencoder <731873664@qq.com>
2016-06-07 16:31:20 +0800

    自动回复控制器

---
+++ WCP/Home/Controller/Weixin/ReplyController.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Author: helen
+ * CreateTime: 2016/06/07 16:30
+ * Description: 自动回复控制器
+ */
+namespace Home\Controller\Weixin;
+
+use Home\Controller\CommonController;
+
+class ReplayController extends CommonController
+{
+
+}
