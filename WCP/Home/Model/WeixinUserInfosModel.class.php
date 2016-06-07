b48a0363ba63839dda63555e8cc3cab2bf7cf6c9
helencoder <731873664@qq.com>
2016-06-07 16:46:47 +0800

    微信用户信息表

--- WCP/Home/Model/WeixinUserInfosModel.class.php
+++ WCP/Home/Model/WeixinUserInfosModel.class.php
@@ -2,5 +2,14 @@
 /**
  * Author: helen
  * CreateTime: 2016/06/07 16:39
- * Description:
+ * Description: 微信用户信息表
  */
+namespace WCP\Model;
+
+use Think\Model;
+
+class WeixinUserInfosModel extends Model
+{
+    protected $tablePrefix = 'wcp_';
+
+}
