cb2534c9a81a3c1fd684a92ead9193d8a38f4493
helencoder <731873664@qq.com>
2016-06-10 16:23:51 +0800

    微信文案控制器

--- WCP/Home/Controller/Weixin/ArticleController.class.php
+++ WCP/Home/Controller/Weixin/ArticleController.class.php
@@ -10,7 +10,58 @@ use Home\Controller\CommonController;
 
 class ArticleController extends CommonController
 {
+    /**
+     * 文案展示界面
+     */
     public function index()
+    {
+
+    }
+
+    /**
+     * 文案创建
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * 文案组合
+     */
+    public function combine()
+    {
+
+    }
+
+    /**
+     * 文案列表
+     */
+    public function lists()
+    {
+
+    }
+
+    /**
+     * 文案预览
+     */
+    protected function preview()
+    {
+
+    }
+
+    /**
+     * 文案群发
+     */
+    protected function sendAll()
+    {
+
+    }
+
+    /**
+     * 文案数据展示
+     */
+    public function data()
     {
 
     }
