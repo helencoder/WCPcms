7d3c8cdb09124fe2fbee91b441545ddb505ecf1d
helencoder <731873664@qq.com>
2016-06-10 16:24:23 +0800

    微信自定义菜单控制器

--- WCP/Home/Controller/Weixin/MenuController.class.php
+++ WCP/Home/Controller/Weixin/MenuController.class.php
@@ -10,8 +10,39 @@ use Home\Controller\CommonController;
 
 class MenuController extends CommonController
 {
+    /**
+     * 自定义菜单展示界面
+     * */
     public function index()
     {
 
     }
+
+    /**
+     * 自定义菜单创建
+     * */
+    protected function create()
+    {
+
+    }
+
+    /**
+     * 自定义菜单查询
+     * */
+    protected function search()
+    {
+
+    }
+
+    /**
+     * 自定义菜单删除
+     */
+    protected function delete()
+    {
+
+    }
+
+    /**
+     * 自定义菜单事件推送
+     */
 }
