33fdc43ceaaadef19c91262a7d91453b8a581bf3
helencoder <731873664@qq.com>
2016-06-15 21:34:29 +0800

    微信公众平台--用户管理

---
+++ WCP/Home/Controller/Weixin/UserController.class.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Author: helen by PhpStorm.
+ * CreatTime: 2016/06/15 21:28
+ * Description: 微信公众平台--用户管理
+ */
+namespace Home\Controller\Weixin;
+
+use Home\Controller\CommonController;
+
+class UserController extends CommonController
+{
+    // 用户标签管理
+    protected function userTags()
+    {
+
+    }
+
+    // 设置用户备注名
+    protected function setUserTags()
+    {
+
+    }
+
+    // 获取用户基本信息
+    protected function getUserInfo()
+    {
+
+    }
+
+    // 获取用户列表
+    protected function getUserList()
+    {
+
+    }
+
+    // 获取用户地理位置
+    protected function getUserLocation()
+    {
+
+    }
+}
