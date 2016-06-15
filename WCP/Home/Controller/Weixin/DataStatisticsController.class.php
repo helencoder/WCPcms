b3cfa4a8c7ac275e963d326b458654c8398244fa
helencoder <731873664@qq.com>
2016-06-15 22:06:51 +0800

    微信公众平台--数据统计

---
+++ WCP/Home/Controller/Weixin/DataStatisticsController.class.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Author: helen
+ * CreatTime: 2016/06/15 22:01
+ * Description: 微信公众平台--数据统计
+ */
+namespace Home\Controller\Weixin;
+
+use Home\Controller\CommonController;
+
+class DataStatisticsController extends CommonController
+{
+    // 用户分析数据接口
+    protected function userStatisticsDataApi()
+    {
+
+    }
+
+    // 图文分析数据接口
+    protected function articleStatisticsDataApi()
+    {
+
+    }
+
+    // 消息分析数据接口
+    protected function msgStatisticsDataApi()
+    {
+
+    }
+
+    // 接口分析数据接口
+    protected function apiStatisticsDataApi()
+    {
+        
+    }
+}
