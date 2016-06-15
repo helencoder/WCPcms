139f6e95a9acc81faf0c6a0cb5a72f6954028abf
helencoder <731873664@qq.com>
2016-06-15 21:53:44 +0800

    微信公众平台--账号管理

--- WCP/Home/Controller/Weixin/AccountManageController.class.php
+++ WCP/Home/Controller/Weixin/AccountManageController.class.php
@@ -1,6 +1,30 @@
 <?php
 /**
- * Author: helen by PhpStorm.
+ * Author: helen
  * CreatTime: 2016/06/15 21:38
- * Description:
+ * Description: 微信公众平台--账号管理
  */
+namespace Home\Controller\Weixin;
+
+use Home\Controller\CommonController;
+
+class AccountManageController extends CommonController
+{
+    // 生成带参数的二维码
+    protected function generateQrcode()
+    {
+
+    }
+
+    // 长链接转短链接接口
+    protected function generateShortUrl()
+    {
+
+    }
+
+    // 微信认证事件推送
+    protected function pushWechatAuthEvent()
+    {
+
+    }
+}
