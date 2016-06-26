819e20efbe659847557602d1b5684f27014a3f34
helen <shixi_hailun3@staff.weibo.com>
2016-06-26 12:02:58 +0800

    展示页面控制器

--- WCP/Home/Controller/Index/DisplayController.class.php
+++ WCP/Home/Controller/Index/DisplayController.class.php
@@ -10,7 +10,7 @@ use Home\Controller\CommonController;
 
 class DisplayController extends CommonController
 {
-    //网站主界面
+    // 网站主界面
     public function main()
     {
         //存储访问用户记录（访问人员记录、时间、IP等）
@@ -20,5 +20,43 @@ class DisplayController extends CommonController
 
     }
 
+    // 前端测试页
+    public function test()
+    {
+        $this->display();
+    }
+
+    // 模板测试页
+    public function template()
+    {
+        // 定制页面基本信息
+        $page = array(
+            'title' 	  => 'WCPcms微信公众平台管理系统',
+            'description' => '定制化、模板化',
+            'author'      => 'helen',
+            /*'icon'        => ''*/  
+        );
+
+        $this->assign('page',$page);
+        $this->display();
+    }
+
+    // cover页面
+    public function cover()
+    {
+        $this->display();
+    }
+
+    // 前端index页面测试
+    public function index()
+    {
+        $this->display();
+    }
+
+    // 后台index页面测试
+    public function admin()
+    {
+        $this->display();
+    }
 
 }
