428012c8b505dbbc7e43b3089ead61bdfe4d54b1
helen <shixi_hailun3@staff.weibo.com>
2016-06-28 22:07:28 +0800

    修改UEditor读取的配置文件位置

--- Website/Public/Ext/UEditor/php/controller.php
+++ Website/Public/Ext/UEditor/php/controller.php
@@ -5,7 +5,9 @@ date_default_timezone_set("Asia/chongqing");
 error_reporting(E_ERROR);
 header("Content-Type: text/html; charset=utf-8");
 
-$CONFIG = json_decode(preg_replace("/\/\*[\s\S]+?\*\//", "", file_get_contents("config.json")), true);
+$config_json = dirname($_SERVER['DOCUMENT_ROOT']) . '/WCP/Home/Conf/config.json';
+$CONFIG = json_decode(preg_replace("/\/\*[\s\S]+?\*\//", "", file_get_contents($config_json)), true);
+//$CONFIG = json_decode(preg_replace("/\/\*[\s\S]+?\*\//", "", file_get_contents("config.json")), true);
 $action = $_GET['action'];
 
 switch ($action) {
