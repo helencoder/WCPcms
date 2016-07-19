48a1b738fa0f728ff2576533c6504aab0d276ee8
helen <shixi_hailun3@staff.weibo.com>
2016-07-19 23:30:33 +0800

    获取验证参数方法

--- WCP/Home/Common/function.php
+++ WCP/Home/Common/function.php
@@ -5,11 +5,11 @@
  * description: 公共函数库
  */
 
-/*
-* 存储访问用户记录函数
-* IP相同，PHPSESSID相同 不进行存储
-* @return interger $id 存储记录id号，存储失败返回null
-* */
+/**
+ * 存储访问用户记录函数
+ * IP相同，PHPSESSID相同 不进行存储
+ * @return interger $id 存储记录id号，存储失败返回null
+ */
 function save_browse_user_records()
 {
     //实例化访问用户记录表
@@ -47,9 +47,9 @@ function save_browse_user_records()
     }
     return $res;
 }
-/*
+/**
  * 获取用户ip，避免使用代理情况，获取真实IP
- * */
+ */
 function getip()
 {
     $unknown = 'unknown';
@@ -75,7 +75,7 @@ function getip()
     return $ip;
 }
 
-/*
+/**
  * 自定义错误处理函数
  * 功能：错误日志记录
  * 功能：错误信息表记录，返回相关信息
@@ -86,8 +86,8 @@ function getip()
     error_line	    可选。规定错误发生的行号。
     error_context	可选。规定一个数组，包含了当错误发生时在用的每个变量以及它们的值。
  *
- * */
-/*
+ */
+/**
  * 文件打开模式(fopen、fclose)
  * 模式	描述
     r	打开文件为只读。文件指针在文件的开头开始。
@@ -98,7 +98,7 @@ function getip()
     w+	打开文件为读/写。删除文件内容或创建新文件，如果它不存在。文件指针在文件开头开始。
     a+	打开文件为读/写。文件中已有的数据会被保留。文件指针在文件结尾开始。创建新文件，如果它不存在。
     x+	创建新文件为读/写。返回 FALSE 和错误，如果文件已存在。
- * */
+ */
 
 function customError($error_level, $error_message, $error_file, $error_line, $error_context)
 {
@@ -128,12 +128,12 @@ function customError($error_level, $error_message, $error_file, $error_line, $er
     }
 }
 
-/*
+/**
  * 缓存函数
  * @param $type 是否缓存，默认缓存$type=1，其他参数为不缓存
  * @param $offset 默认缓存时间一小时
  *
- * */
+ */
 function cache($type = 1, $offset = 86400)
 {
     switch ($type) {
@@ -154,9 +154,9 @@ function cache($type = 1, $offset = 86400)
     }
 }
 
-/*
+/**
  * 获取当前页面的完整url
- * */
+ */
 function getCurrentUrl()
 {
     //当前页面的url
@@ -164,7 +164,7 @@ function getCurrentUrl()
     return $url;
 }
 
-/*
+/**
  * 功能：接口请求函数
  * 参数：url，[data](通过是否传入data判断其为get请求还是post请求)
  * 返回：json数据
@@ -203,10 +203,10 @@ function request($url, $data = null)
     return ($res);   //返回json数据
 }
 
-/*
+/**
  * 随机数生成函数
  * 可以指定生成随机数的长度,默认16位
- * */
+ */
 function getRandomCode($length = 16)
 {
     $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -216,4 +216,41 @@ function getRandomCode($length = 16)
         $code .= $chars[$tip];
     }
     return $code;
+}
+
+/**
+ * 获取参数数组、用于正则等验证(包括大写字母数组、小写字母数组、数字数组)
+ * 方法：
+ *      chr()函数可把ASCII码转换成普通字符
+ *      1.0-9的ASCII码为48-57
+ *      2.a-z的ASCII码为97-122
+ *      3.A-Z的ASCII码为65-90
+ */
+function verifyArgs()
+{
+    $args = array(
+        'lowerCase' => array(),
+        'upperCase' => array(),
+        'number' => array()
+    );
+    // 处理方式1 (直接字符串增加)
+    /*for ($i = 'a', $j = 'A'; $i < 'z' , $j < 'Z'; $i++, $j++) {
+        array_push($args['lowerCase'], $i);
+        array_push($args['upperCase'], $j);
+    }
+    array_push($args['lowerCase'], 'z');
+    array_push($args['upperCase'], 'Z');*/
+
+    // 处理方式2 (ASCII码转换)
+    for ($i = 65, $j = 97; $i < 91 ,$j < 123; $i++, $j++) {
+        array_push($args['upperCase'], chr($i));
+        array_push($args['lowerCase'], chr($j));
+    }
+
+    // 处理数字数组(此处貌似有更好方法)
+    for ($k = 0; $k <= 9; $k++) {
+        array_push($args['number'], $k);
+    }
+
+    return $args;
 }
