90923ef4354ef8793a3f037909faf6031a0e4216
helen <shixi_hailun3@staff.weibo.com>
2016-07-19 23:25:49 +0800

    网站用户记录表

---
+++ SQL/wcp_website_user.sql
@@ -0,0 +1,33 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50505
+Source Host           : localhost:3306
+Source Database       : wcp
+
+Target Server Type    : MYSQL
+Target Server Version : 50505
+File Encoding         : 65001
+
+Date: 2016-07-19 23:21:40
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for wcp_website_user
+-- ----------------------------
+DROP TABLE IF EXISTS `wcp_website_user`;
+CREATE TABLE `wcp_website_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL COMMENT '用户名，唯一',
+  `password` varchar(255) NOT NULL COMMENT '密码',
+  `email` varchar(255) NOT NULL COMMENT '用户邮箱，唯一',
+  `authority` tinyint(4) DEFAULT '3' COMMENT '用户权限，管理员：0，付费用户：1，试用用户：2，游客：3',
+  `create_time` datetime DEFAULT NULL COMMENT '用户创建时间',
+  `modify_time` datetime DEFAULT NULL COMMENT '用户修改时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
