e44549f4ebcee05644b28fa8d7b9265773d777f5
helen <shixi_hailun3@staff.weibo.com>
2016-08-16 23:37:41 +0800

    产品表model&权限表model

---
+++ WCP/Home/Model/AuthorityModel.class.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Author：helen
+ * CreateTime: 2016/08/16 23:31
+ * Description：
+ */
---
+++ WCP/Home/Model/ProductModel.class.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Author：helen
+ * CreateTime: 2016/08/16 23:30
+ * Description：
+ */
