df4ed066354236fc84974b4c5cd743f240cd3c48
helen <shixi_hailun3@staff.weibo.com>
2016-08-16 23:39:12 +0800

    产品表model&权限表model

--- WCP/Home/Model/AuthorityModel.class.php
+++ WCP/Home/Model/AuthorityModel.class.php
@@ -2,5 +2,42 @@
 /**
  * Author：helen
  * CreateTime: 2016/08/16 23:31
- * Description：
+ * Description：权限表model
  */
+namespace Home\Model;
+
+use Think\Model;
+
+class AuthorityModel extends Model
+{
+    protected $tablePrefix = 'wcp_';
+
+    /**
+     * 实例化当前数据表
+     */
+    protected function db_table()
+    {
+        $db_table = D('authority');
+        return $db_table;
+    }
+
+    /**
+     * 获取当前数据表中的所有数据
+     */
+    public function getData()
+    {
+        $db = $this->db_table();
+        $data = $db->select();
+        return $data;
+    }
+
+    /**
+     * 获取当前数据表中的数据条数
+     */
+    public function getCount()
+    {
+        $db = $this->db_table();
+        $count = $db->count();
+        return $count;
+    }
+}
