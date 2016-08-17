fe2f5f4a60094ea07bf3fefe726d797a0e75795a
helen <shixi_hailun3@staff.weibo.com>
2016-08-17 09:40:19 +0800

    平台用户表修改

--- SQL/wcp_website_user.sql
+++ SQL/wcp_website_user.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2016-07-19 23:21:40
+Date: 2016-08-17 09:38:33
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,8 +23,9 @@ CREATE TABLE `wcp_website_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL COMMENT '用户名，唯一',
   `password` varchar(255) NOT NULL COMMENT '密码',
+  `openid` char(30) DEFAULT NULL,
   `email` varchar(255) NOT NULL COMMENT '用户邮箱，唯一',
-  `authority` tinyint(4) DEFAULT '3' COMMENT '用户权限，管理员：0，付费用户：1，试用用户：2，游客：3',
+  `type` tinyint(4) DEFAULT '3' COMMENT '用户权限，管理员：0，付费用户：1，试用用户：2，游客：3',
   `create_time` datetime DEFAULT NULL COMMENT '用户创建时间',
   `modify_time` datetime DEFAULT NULL COMMENT '用户修改时间',
   PRIMARY KEY (`id`),
